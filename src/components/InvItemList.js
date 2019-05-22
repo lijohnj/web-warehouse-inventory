ca499b1b2c2e3bc60de709b8a228a2e1eb8ebc7f
lijohnj <lijohnj@gmail.com>
2019-05-22 17:41:02 -0400

    config

--- src/components/InvItemList.js
+++ src/components/InvItemList.js
@@ -161,7 +161,7 @@ class InvItemList extends React.Component {
       page: pageNum,
     });
 
-    axios.get('http://localhost:8000/items')
+    axios.get('https://webwarehouseinventory.herokuapp.com:8000/items')
       .then(response => {
         this.setState({ rows: response.data, page: parseInt(pageNum) });
       })
